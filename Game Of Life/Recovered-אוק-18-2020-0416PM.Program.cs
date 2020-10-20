c453ad1eeb4511aff9ba2b025f3e046f4fd95fc9
Raziel Zohar <albrzohar@gmail.com>
2020-10-20 16:14:23 +0300

    Program Structure

---
+++ Game Of Life/Recovered-אוק-18-2020-0416PM.Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Game_Of_Life
+{
+    class Field {
+        private Array[,] FieldSize;
+        private Array[,] FieldSize
+        {
+            get {return FieldSize; }
+            set {FieldSize = value; }
+        } 
+    }
+    class Cell {
+        public void startGame() { }
+
+        public virtual int FindNumOfNeighbors() { }
+    }
+    class Red:Cell {
+        public override int FindNumOfNeighbors() { }
+        public Boolean isRedDies() { }
+    }
+    class Blue:Cell {
+        public int FindNumOfBlueNeighbors() { }
+        public int FindNumOfRedNeighbors() { }
+        public Boolean isBlueDies() { }
+    }
+    class Unoccuppied:Cell {
+        public override int FindNumOfNeighbors() { }
+        public Boolean isNewCellBorn() { }
+    }
+    class Cells_Group
+    {
+        private Array[] Cells;
+
+        public Boolean IsWin(Array[] Cells)
+        {
+            foreach (Cell cell in Cells)
+            {
+                if (Cells[Cell] != null)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public Array[] Cells {
+            get {
+                return Cells;
+            }
+            set {
+                Cells = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
