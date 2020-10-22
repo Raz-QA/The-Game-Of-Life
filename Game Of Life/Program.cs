00dc91676431abbbcc46422a419cf13745943f3e
Raziel Zohar <albrzohar@gmail.com>
2020-10-22 19:32:32 +0300

    StartGame

--- Game Of Life/Program.cs
+++ Game Of Life/Program.cs
@@ -1,109 +1,150 @@
 ﻿using System;
 
+
 namespace Game_Of_Life
 {
     class Field {
-        private Array[,] FieldSize;
-        private Array[,] FieldSize
+        public Cell[,] FieldSize = new Cell[10,10];
+        private Random randomNumberX = new Random();
+        private Random randomNumberY = new Random();
+        public void startGame()
         {
-            get {return FieldSize; }
-            set {FieldSize = value; }
-        } 
+            // בצורה רנדומלית נבחרים 3 מקומות בלוח המשחק שבהם
+            //       שלוש תאים יהיו כחולים ושלוש תאים אדומים  
+            for (int i = 0; i < FieldSize.Length; i++)
+            {
+                for (int j = 0; i < FieldSize.Length; j++)
+                {
+                    FieldSize[i, j] = new Unoccupied();
+                }
+
+            }
+            for(int i = 1; i<=3; i++)
+            {
+                FieldSize[randomNumberX, randomNumberY] = new Blue;
+                
+            }
+            for (int i = 1; i <= 3; i++)
+            {
+                FieldSize[randomNumberX, randomNumberY] = new Red;
+
+            }
+
+        }
+         public Boolean IsWin(Cell[,] FieldSize)
+            {
+                {
+                    // בודק האם כל הלוח פנוי במידה וכן מנצח
+                    for (int i = 0; i < FieldSize.Length; i++)
+                    {
+                        for (int j = 0; j < FieldSize.Length; i++)
+                        {
+                            if (FieldSize[i, j] != null)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    return true;
+                }
+            }
+
+
     }
     class Cell {
-        public void startGame() {
-            // בצורה רנדומלית נבחרים 3 מקומות בלוח המשחק שבהם
-            //        שלוש תאים יהיו כחולים ושלוש תאים אדומים  
-        }
+        protected int neighbors = 0;
+        protected int redNeighbors = 0;
+        protected int blueNeighbors = 0;
 
         public virtual int FindNumOfNeighbors() {
-            foreach (Cell cell in Cells)
+            foreach (Cell cell in FieldSize)
             {
                 // לבדוק את הבא אחריו זה שלפניו מעליו מתחתיו ובאלכסונים
                 // ולבדוק כמה יש שכנים שהם לא פנויים ולשמור בתוך משתנה
             }
+            return neighbors;
         }
     }
     class Red:Cell {
         public override int FindNumOfNeighbors() {
-            foreach (Red red in Cells) {
-               // לבדוק את הבא אחריו זה שלפניו מעליו מתחתיו ובאלכסונים
-               // ולבדוק כמה יש שכנים שהם לא פנויים ולשמור בתוך משתנה
+           
+            foreach (Red red in FieldSize) {
+               
+                // לבדוק את הבא אחריו זה שלפניו מעליו מתחתיו ובאלכסונים
+                // ולבדוק כמה יש שכנים שהם לא פנויים ולשמור בתוך משתנה
+               
             }
+            return neighbors;
         }
-        public override int FindNumOfRedNeighbors()
+        public int FindNumOfRedNeighbors()
         {
-            foreach (Red red in Cells)
+            foreach (Red red in FieldSize)
             {
                 // לבדוק את הבא אחריו זה שלפניו מעליו מתחתיו ובאלכסונים
                //           ולבדוק כמה יש שכנים אדומים ולשמור בתוך משתנה
             }
+            return redNeighbors;
         }
-        public Boolean isRedDies() { 
-            if (//redNeighbors == 0 || neighbors <= 1 || neighbors >= 4) { אדום נהפך להיות תא פנוי }
-     
+        public Boolean isRedDies() {
+            if (redNeighbors == 0 || neighbors <= 1 || neighbors >= 4) {
+                //  אדום נהפך להיות תא פנוי 
+                return true;
+            }
+            return false;
 }
     }
-    class Blue:Cell {
+    class Blue:Cell { 
         public int FindNumOfBlueNeighbors() {
-            foreach (Blue blue in Cells)
+            foreach (Blue blue in FieldSize)
             {
                 //       ,לבדוק את הבא אחריו זה שלפניו מעליו מתחתיו ובאלכסונים
                 //                 ולבדוק כמה יש שכנים כחולים ולשמור בתוך משתנה 
+            }
+            return blueNeighbors;
         }
         public int FindNumOfRedNeighbors() {
-            foreach (Blue blue in Cells)
+            foreach (Blue blue in FieldSize)
             {
                 //      ,לבדוק את הבא אחריו זה שלפניו מעליו מתחתיו ובאלכסונים
                 //                ולבדוק כמה יש שכנים אדומים ולשמור בתוך משתנה
             }
+            return redNeighbors;
         }
         public Boolean isBlueDies() {
-           if (//blueNeighbors != 1 || redNeighbors == 0 || redNeighbors > 3 ) {  כחול נהפך להיות תא פנוי }
+           if (blueNeighbors != 1 || redNeighbors == 0 || redNeighbors > 3 ) {
+                //כחול נהפך להיות תא פנוי 
+                return true;
+            }
+            return false;
             }
     }
-    class Unoccuppied:Cell {
+    class Unoccupied:Cell {
         public override int FindNumOfNeighbors() {
-            foreach (Unoccuppied unoccuppied in Cells)
+            foreach (Unoccupied unoccuppied in FieldSize)
             {
                 //   ,לבדוק את הבא אחריו זה שלפניו מעליו מתחתיו ובאלכסונים
                 //    ולבדוק כמה יש שכנים שהם לא פנויים ולשמור בתוך משתנה 
             }
+            return neighbors;
         }
         public Boolean isNewCellBorn() {
-                if (// neighbors == 3) {  מתחלף התא הפנוי באדום או כחול בהסתברות של 0.5}
+            if (neighbors == 3)
+            {
+                // מתחלף התא הפנוי באדום או כחול בהסתברות של 0.5
+                return true;
+            }
+            return false;
             }
     }
     class Cells_Group
     {
-        private Array[] Cells;
-
-        public Boolean IsWin(Array[] Cells)
-        {
-          // בודק האם כל הלוח פנוי במידה וכן מנצח
-            foreach (Cell cell in Cells)
-            {
-                if (Cells[Cell] != null)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-        public Array[] Cells {
-            get {
-                return Cells;
-            }
-            set {
-                Cells = value;
-            }
-        }
+     
     }
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
